f7f78205e7efcf19e705bb3e33ff747eb8c651ed
camp7590 <camp7590@ISS2V3QKH2.scdsb.pri>
2018-01-19 12:00:43 -0500

    jklkl

---
+++ src/AbsStore.java
@@ -0,0 +1,19 @@
+
+import javax.swing.ImageIcon;
+
+
+abstract public class AbsStore {
+    protected String name;
+    protected int price;
+    protected int buy;
+    protected String catagory;
+    protected ImageIcon image;
+   
+    public AbsStore(boolean b, String n, int p, String c,ImageIcon i){
+        
+    }
+    
+    public String toString(){
+       
+      }
+}
