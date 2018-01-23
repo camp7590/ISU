ccf5f227bfe66b5216ddeed650a1e8fc894d9702
campb <campb@DESKTOP-TF2IMQV>
2018-01-23 01:20:13 -0600

    k

--- src/AbsStore.java
+++ src/AbsStore.java
@@ -1,19 +1,50 @@
 
 import javax.swing.ImageIcon;
 
-
 abstract public class AbsStore {
+
     protected String name;
     protected int price;
     protected int buy;
-    protected String catagory;
+    protected int sell;
+    protected String facts;
     protected ImageIcon image;
-   
-    public AbsStore(boolean b, String n, int p, String c,ImageIcon i){
-        
+
+    String d[] = {"Cheese", "Milk", "Yogurt", "Butter"};
+    String m[] = {"Fish", "Chicken", "Pork", "Beef"};
+    String f[] = {"Apple", "Banana", "Strawberry", "Grapes"};
+
+    public AbsStore() {
+
     }
-    
-    public String toString(){
-       
-      }
+
+    public AbsStore(boolean b, String n, int p, String c, ImageIcon i) {
+
+    }
+
+    public String dairy() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(d[i]);
+        }
+        return null;
+    }
+
+    public String Meat() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(m[i]);
+        }
+        return null;
+    }
+
+    public String Fruits() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(f[i]);
+        }
+        return null;
+    }
+
+    public ImageIcon getImage() {
+        return image;
+    }
+
 }
